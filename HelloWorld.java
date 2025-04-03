a6733345e160516699d678364f5bbe78636fa1af
bacan919200 <baca.n919200@stu.SanJac.edu>
2025-04-03 05:40:48 +0000

    Lab1

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld 
+/* This is the class header. It is showing that this program can be publicly accessed and the name of the class is HelloWorld*/
+{
+    public static void main(String[] args) 
+    /*The method header contains information about how other methods can interact with it. It is showing that the program is publicly accessed, without an object, and the main method does not return a value when called*/
+    {
+        System.out.println("Hello, World!");
+        System.out.println("Name: Nidia Baca");
+        System.out.println("Favorite Food: Pizza");
+        System.out.println("Someone I'd like to meet: Jesus");
+        System.out.println("Least Favorite Chore: Washing Dishes");
+        System.out.println("What I'd rather be doing: Sleep");
+        /* This Java statement helps output exactly what is entered between the quotations */
+        /*What does the main() method represent in a Java program? This line containg information about how other methods can interact with it. */
+        /* Why is System.out.println() used to display output? System is a class. out is a property of the System class. println() is a method that prints the argument and moves the curser to a new line*/
+        /* What is the purpose of comments in the program? The purpose of comments is for documentation, notes, or clarification for another person reviewing the author's work. */
+    }
+}
